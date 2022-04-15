111b6dcdf6e53863bfebcda7f2aba15fb5a07e78
trale-ogilvy <thanhtra.le@ogilvy.com>
2022-04-15 17:04:06 +0700

    init

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'a-frame',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    vendor: ['aframe']
+  },
+
+  vue: {
+    config: {
+      ignoredElements: [
+        'a-scene',
+        'a-assets',
+        'a-sky',
+        'a-entity',
+        'a-plane',
+        'a-camera',
+        'a-cursor',
+        'a-cube',
+        'a-asset-item'
+      ]
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <a-scene>
+      <a-assets>
+        <img id="sky" src="images/59c90f262e7bef8a1f000214.png" alt="" />
+        <a-asset-item id="groot" src="model/groot.gltf"></a-asset-item>
+      </a-assets>
+      <a-sky src="#sky"></a-sky>
+      <!-- <a-entity gltf-model="#mastercheif"></a-entity> -->
+      <!-- <a-entity
+        id="groot-model"
+        gltf-model="url(model/groot.gltf)"
+        position="-7 0 0"
+        rotation="0 90 0"
+        animation-mixer="clip:mixamo.com"
+      ></a-entity> -->
+      <a-entity
+        id="deadpool"
+        gltf-model="url(model/deadpool_dancing/scene.gltf)"
+        position="-7 0 0"
+        rotation="0 90 0"
+        animation-mixer="clip:mixamo.com"
+      ></a-entity>
+
+      <a-entity rotation="0 90 0">
+        <a-camera wasd-controls="enabled:false"></a-camera>
+      </a-entity>
+    </a-scene>
+    <div class="controls">
+      <div ref="KeyW" class="button" @click="onClick('KeyW')">W</div>
+      <div ref="KeyA" class="button" @click="onClick('KeyA')">A</div>
+      <div ref="KeyS" class="button" @click="onClick('KeyS')">S</div>
+      <div ref="KeyD" class="button" @click="onClick('KeyD')">D</div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.controls {
+  position: fixed;
+  bottom: 40px;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+  display: flex;
+  justify-content: space-around;
+}
+.button {
+  width: 100px;
+  height: 100px;
+  border-radius: 100%;
+  color: #fff;
+  border: 4px solid #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 50px;
+  font-weight: 700;
+  cursor: pointer;
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none;
+}
+.button.active {
+  color: red;
+  border-color: red;
+}
+</style>
+<script>
+import Vue from "vue";
+import AFRAME from "aframe";
+require("aframe-extras");
+Vue.use(AFRAME);
+export default {
+  components: {},
+  mounted() {
+    
+    // console.log(deadpool.components["animation-mixer"].attrValue);
+    document.addEventListener("keypress", (e) => {
+      this.onClick(e.code)
+    });
+  },
+  methods: {
+    onClick(key) {
+      this.$refs[key]?.classList.add("active");
+      setTimeout(() => {
+        this.$refs[key]?.classList.remove("active");
+      }, 100);
+      const deadpool = this.$el.querySelector("#deadpool");
+      switch (key) {
+        case "KeyW":
+          deadpool.setAttribute("animation-mixer", { clip: "chick" });
+          return;
+        case "KeyA":
+          deadpool.setAttribute("animation-mixer", { clip: "gang" });
+          return;
+        case "KeyS":
+          deadpool.setAttribute("animation-mixer", { clip: "hip" });
+          return;
+        case "KeyD":
+          deadpool.setAttribute("animation-mixer", { clip: "ymca" });
+          return;
+        case "Space":
+          deadpool.setAttribute("animation-mixer", { clip: "mixamo.com" });
+          return;
+        default:
+          return;
+      }
+    },
+  },
+};
+</script>
